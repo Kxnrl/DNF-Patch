1d787337404e0de54aab43e43bb69163cc7d3568
Kyle <kyle@kxnrl.com>
2019-03-09 19:14:30 +0800

    Program

---
+++ DNF-Tools/Program.cs
@@ -0,0 +1,169 @@
+﻿using System;
+
+namespace DNF_Tools
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "DNF-Patch v1.0        By Kyle";
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine("系统初始化...");
+            Console.WriteLine("侦测DNF文件位置...");
+
+            var path = Utils.Path.Find();
+
+            if (string.IsNullOrEmpty(path))
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("您的电脑尚未安装DNF...");
+                Console.ReadKey();
+                goto done;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("DNF路径为: " + path + Environment.NewLine);
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("=========================================");
+            Console.WriteLine("正在检查游戏进程...");
+            Utils.Terminator.FindAndKill();
+
+            if ( Environment.OSVersion.Version.Major == 10 || 
+                (Environment.OSVersion.Version.Major == 6 && Environment.OSVersion.Version.Minor >= 2)
+               )
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("=========================================");
+                Console.WriteLine("正在检查windows8/8.1/10蓝屏修复..." + Environment.NewLine);
+                Utils.Win32Api.AutoMaintenance(false);
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("=========================================");
+            Console.WriteLine("正在检查NPK补丁..." + Environment.NewLine);
+
+            var patches = Utils.Patch.Check(path);
+
+            if (patches.Count > 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("将安装下列补丁:");
+
+                foreach (var patch in patches)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("[{0}]\t{1}", patch.Type, patch.Name);
+                }
+
+                Console.WriteLine("");
+            }
+            else
+            {
+                // skip
+                goto step2;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("初始化下载队列...");
+
+            bool installed = false;
+
+            foreach (var patch in patches)
+            {
+                try
+                {
+                    Utils.Downloader.Download(System.IO.Path.Combine(path, "ImagePacks2"), patch.File);
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.WriteLine("安装成功 ->\t[{0}]\t{1}", patch.Type, patch.Name);
+                    installed = true;
+                }
+                catch (Exception e)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("无法下载补丁 [{0}] -> 异常: {1}", patch.File, e.Message);
+                }
+            }
+
+            if (installed)
+            {
+                try
+                {
+                    System.Diagnostics.Process.Start("explorer.exe", "\""+ System.IO.Path.Combine(path, "ImagePacks2") + "\"");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(Environment.NewLine + "补丁操作已完成..." + Environment.NewLine);
+                }
+                catch { }
+            }
+            else
+            {
+
+            }
+
+            step2:
+            Console.WriteLine("=========================================");
+            Console.WriteLine("正在检查优化工具..." + Environment.NewLine);
+
+            // System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory), "Tool2.exe");
+            var dirt = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Kxnrl", "DNF");
+            var tool = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Kxnrl", "DNF", "Tool2.exe");
+
+            if (!System.IO.Directory.Exists(dirt))
+            {
+                try
+                {
+                    System.IO.Directory.CreateDirectory(dirt);
+                }
+                catch (Exception e)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("无法创建目录 -> 异常: {0}", e.Message);
+                    goto done;
+                }
+            }
+
+            if (!System.IO.File.Exists(tool))
+            {
+                try
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("正在下载优化工具...");
+                    Utils.Downloader.Download(dirt, "Tool2.exe");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("下载工具完成...");
+                }
+                catch (Exception e)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("无法下载工具 -> 异常: {0}", e.Message);
+                    goto done;
+                }
+            }
+
+            try
+            {
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("请在新打开的窗口中操作..." + Environment.NewLine + "完成后请直接关闭新窗口");
+                System.Threading.Thread.Sleep(500);
+                System.Diagnostics.Process proc = new System.Diagnostics.Process();
+                proc.StartInfo.FileName = tool;
+                proc.Start();
+                System.Threading.Thread.Sleep(500);
+                Utils.Win32Api.FocusWindow(proc.MainWindowHandle);
+                proc.WaitForExit();
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("无法启动工具 -> 异常: {0}", e.Message);
+            }
+
+            done:
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(Environment.NewLine + "所有操作已完成..." + Environment.NewLine + "按下任意键退出...");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DNF-Tools/Utils.cs
@@ -0,0 +1,342 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+
+namespace DNF_Tools.Utils
+{
+    class Path
+    {
+        public static string Find()
+        {
+            var ret = string.Empty;
+
+            if (Reg(ref ret))
+            {
+                // find
+                return ret;
+            }
+
+            if (Default(ref ret))
+            {
+                // find 
+                return ret;
+            }
+
+            if (Search(ref ret))
+            {
+                // find
+                return ret;
+            }
+
+            return string.Empty;
+        }
+
+        private static bool Reg(ref string path)
+        {
+            try
+            {
+                var reg = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64).OpenSubKey(@"SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\地下城与勇士");
+                var val = reg.GetValue("InstallSource", string.Empty, RegistryValueOptions.DoNotExpandEnvironmentNames).ToString();
+
+                if (!string.IsNullOrEmpty(val))
+                {
+                    path = val;
+                    return true;
+                }
+
+                return false;
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("异常: " + e.Message);
+            }
+
+            return false;
+        }
+
+        private static bool Default(ref string path)
+        {
+            var list = new string[]
+            {
+                "地下城与勇士",
+                System.IO.Path.Combine("Program Files (x86)", "地下城与勇士"),
+                System.IO.Path.Combine("Program Files", "地下城与勇士"),
+                System.IO.Path.Combine("Program Files (x86)", "腾讯游戏", "地下城与勇士"),
+                System.IO.Path.Combine("网络游戏", "地下城与勇士"),
+            };
+
+            foreach (DriveInfo driver in DriveInfo.GetDrives().Where(x => x.IsReady == true))
+            {
+                try
+                {
+                    foreach (var c in list)
+                    {
+                        var p = System.IO.Path.Combine(driver.RootDirectory.FullName, c);
+
+                        if (!Directory.Exists(p))
+                        {
+                            //exists?
+                            continue;
+                        }
+
+                        if (Directory.GetFiles(p, "DNF.exe", SearchOption.TopDirectoryOnly).Length > 0)
+                        {
+                            // find
+                            path = p;
+                            return true;
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine("异常: " + e.Message);
+                }
+            }
+
+            return false;
+        }
+
+        private static bool Search(ref string path)
+        {
+            foreach (DriveInfo driver in DriveInfo.GetDrives().Where(x => x.IsReady == true))
+            {
+                try
+                {
+                    Foreach(driver.RootDirectory.FullName, ref path);
+
+                    if (!string.IsNullOrEmpty(path))
+                    {
+                        // done
+                        return true;
+                    }
+                }
+                catch (UnauthorizedAccessException) { }
+                catch (Exception e)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine("异常: " + e.Message);
+                }
+            }
+
+            return false;
+        }
+
+        private static void Foreach(string path, ref string find)
+        {
+            var c = 0;
+            var d = Directory.GetDirectories(path);
+
+            foreach (var dir in d)
+            {
+                if (dir.Contains("\\ImagePacks2") || dir.Contains("\\SoundPacks"))
+                {
+                    // 资源文件夹
+
+                    if (++c == 2)
+                    {
+                        if (Directory.GetFiles(path, "DNF.exe", SearchOption.TopDirectoryOnly).Length > 0)
+                        {
+                            find = path;
+                            return;
+                        }
+                    }
+                }
+            }
+
+            foreach (var dir in d)
+            {
+                try
+                {
+                    Foreach(dir, ref find);
+                }
+                catch (UnauthorizedAccessException) { }
+                catch (Exception e)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine("异常: " + e.Message);
+                }
+            }
+        }
+    }
+
+    class Patch
+    {
+        public struct Patches
+        {
+            public string Type;
+            public string Name;
+            public string File;
+
+            public Patches(string t, string n, string f)
+            {
+                Type = t;
+                Name = n;
+                File = f;
+            }
+        }
+
+        private static readonly string[] _patches = new string[]
+        {
+            
+            "a(其他补丁-空中坐标)",
+            "a(其他补丁-蓝冰翼喷射器)",
+            "a(其他补丁-去失明、过图黑屏、死亡黑屏)",
+            "a(其他补丁-透明血条)",
+            "a(超时空漩涡-便利补丁)",
+            "a(泰波尔斯-暗能量优化)",
+            "a(泰波尔斯-翅膀怪)",
+            "a(泰波尔斯-翅膀优化)",
+            "a(泰波尔斯-地图优化)",
+            "a(泰波尔斯-斗气释放)",
+            "a(泰波尔斯-风阻优化)",
+            "a(泰波尔斯-怪暗能量以及移动风优化)",
+            "a(泰波尔斯-切图优化)",
+            "a(泰波尔斯-太空列车)",
+            "a(泰波尔斯-终极优化)"
+        };
+
+        public static List<Patches> Check(string path)
+        {
+            List<Patches> patches = new List<Patches>();
+
+
+            foreach (var file in _patches)
+            {
+                if (File.Exists(System.IO.Path.Combine(path, "ImagePacks2", file + ".NPK")))
+                {
+                    // 已安装
+                    continue;
+                }
+
+                var data = file.Split('-');
+
+                if (data.Length != 2)
+                {
+                    // ??
+                    continue;
+                }
+
+                patches.Add(new Patches(data[0].Replace("a(", ""), data[1].Replace(")", ""), file + ".NPK"));
+            }
+
+            return patches;
+        }
+    }
+
+    class Downloader
+    {
+        const string url_pref = "https://static.kxnrl.com/DNF/";
+
+        public static void Download(string path, string file)
+        {
+            using (var client = new WebClient())
+            {
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+                client.DownloadFile(url_pref + file, System.IO.Path.Combine(path, file));
+            }
+        }
+    }
+
+    class Win32Api
+    {
+        [DllImport("user32.dll")]
+        internal static extern IntPtr SetForegroundWindow(IntPtr hwnd);
+
+        [DllImport("user32.dll")]
+        internal static extern bool ShowWindow(IntPtr hwnd, int cmd);
+
+        public static void FocusWindow(IntPtr window)
+        {
+            ShowWindow(window, 5);
+            SetForegroundWindow(window);
+        }
+
+        public static void AutoMaintenance(bool enabled = false)
+        {
+            try
+            {
+                var reg = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\Maintenance", true);
+
+                var set = true;
+
+                var val = enabled ? 0 : 1;
+
+                foreach (var key in reg.GetValueNames())
+                {
+                    if (key.Equals("MaintenanceDisabled"))
+                    {
+                        if (reg.GetValueKind("MaintenanceDisabled") != RegistryValueKind.DWord)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Cyan;
+                            Console.WriteLine("侦测到无效的数据类型...");
+                            reg.DeleteValue("MaintenanceDisabled");
+                        }
+                        else if (int.Parse(reg.GetValue("MaintenanceDisabled").ToString()) == val)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Cyan;
+                            Console.WriteLine("侦测到值[{0}/{1}]...", reg.GetValue("MaintenanceDisabled").ToString(), val);
+                            set = false;
+                        }
+
+                        break;
+                    }
+                }
+
+                if (set)
+                {
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.WriteLine("重新设置注册表值...");
+                    reg.SetValue("MaintenanceDisabled", 1);//, RegistryValueKind.DWord
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("已修复Windows10挂机蓝屏..." + Environment.NewLine + "操作将在下次重启后生效...");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("异常: " + e.Message + Environment.NewLine + e.StackTrace);
+            }
+            finally
+            {
+                Console.WriteLine("");
+            }
+        }
+    }
+
+    class Terminator
+    {
+        public static void FindAndKill()
+        {
+            try
+            {
+                var list = new string[] { "CrossProxy", "TPHelper", "TQMCenter", "tgp_gamead", "GameLoader", "DNF" };
+
+                foreach (var n in new string[] { "CrossProxy", "TPHelper", "TQMCenter", "tgp_gamead", "GameLoader", "DNF" })
+                {
+                    foreach (var p in Process.GetProcessesByName(n))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.WriteLine("结束进程 {0} -> {1}", p.ProcessName, p.Id);
+                        p.Kill();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("异常: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("");
+            }
+        }
+    }
+}
